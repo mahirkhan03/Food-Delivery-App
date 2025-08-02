3c31233b7ccd89d9032de11004d93d9286bda1ad
mahirkhan03 <mahirkhan0707@gmail.com>
2025-08-02 14:59:46 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,11 @@ const port = 4000
 
 app.use(express.json())
 app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
