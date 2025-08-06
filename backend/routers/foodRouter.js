c8cb51ca5beec7eac8c894c8a913ee9f15131527
mahirkhan03 <mahirkhan0707@gmail.com>
2025-08-06 12:33:37 +0530

    first commit

--- backend/routers/foodRouter.js
+++ backend/routers/foodRouter.js
@@ -4,6 +4,19 @@ import multer from 'multer'
 
 const foodRouter = express.Router();
 
-foodRouter.post("/add", addFood)
+//Image Storage Engine
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+        return cb(null, `${Date.now()}${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+foodRouter.post("/add", upload.single("image"), addFood)
+
+
+
 
 export default foodRouter;
