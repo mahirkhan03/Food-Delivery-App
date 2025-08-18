99b2441a3071063dd3d1066c8182c48681cf94a1
mahirkhan03 <mahirkhan0707@gmail.com>
2025-08-18 13:48:31 +0530

    first commit

---
+++ admin/src/component/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./NavBar.css"
+const NavBar = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default NavBar
---
+++ admin/src/component/Sidebar/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./SideBar.css"
+const SideBar = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default SideBar
