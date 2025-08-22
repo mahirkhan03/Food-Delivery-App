355cd371ca3a3aa1bb3a266739820a041abe65cd
mahirkhan03 <mahirkhan0707@gmail.com>
2025-08-22 16:44:29 +0530

    first commit

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -5,8 +5,8 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 
 
-const Add = () => {
-    const url = "http://localhost:4000"
+const Add = ({ url }) => {
+  
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
         name: "",
