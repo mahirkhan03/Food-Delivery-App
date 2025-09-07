7c1035534df7d161b629ebd46e6a4b76d9815783
mahirkhan03 <mahirkhan0707@gmail.com>
2025-09-07 12:56:02 +0530

    first commit

--- frontend/src/component/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/component/LoginPopUp/LoginPopUp.jsx
@@ -35,6 +35,10 @@ const LoginPopUp = ({ setShowLogin }) => {
         if (response.data.success) {
             setToken(response.data.token);
             localStorage.setItem("token",response.data.token)
+            setShowLogin(false )
+        }
+        else{
+            alert(response.data.message)
         }
     }
 
--- frontend/src/component/NavBar/NavBar.jsx
+++ frontend/src/component/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("home");
 
-    const { getTotalCartAmount } = useContext(StoreContext);
+    const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
 
     return (
         <div className='navbar'>
@@ -23,9 +23,17 @@ const NavBar = ({ setShowLogin }) => {
                 <img src={assets.search_icon} alt="" />
                 <div className="nav-search-icon">
                     <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                    <div className={getTotalCartAmount()===0? "" : "dot"}></div>
+                    <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
                 </div>
-                <button onClick={() => setShowLogin(true)}>sign in</button>
+                {!token?<button onClick={() => setShowLogin(true)}>sign in</button>
+                :<div className='navbar-profile'>
+                    <img src={assets.profile_icon} alt="" />
+                    <ul className="nav-profile-dropdown">
+                        <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                        <hr />
+                        <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+                    </ul>
+                    </div>}
             </div>
         </div>
     )
