f174a26b33e64e1c2ed4fe03b880ea7eddc01aa5
mahirkhan03 <mahirkhan0707@gmail.com>
2025-09-08 13:34:48 +0530

    first commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -35,6 +35,12 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"))
+        }
+    }, [])
+
     const contextValue = {
         food_list,
         cartItems,
